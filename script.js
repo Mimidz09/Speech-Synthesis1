55cc4f2e9cc0b68e9a47a9cff43f3c0ec4742117
Mimidz09 <mebrouka.rbh85@gmail.com>
2022-11-18 10:28:48 +0100

    fin du projet

---
+++ script.js
@@ -0,0 +1,34 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+
+function speaker(start = true) {
+  msg.text = document.querySelector("[name='text']").value;
+  speechSynthesis.cancel();
+  if (start) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function changeVoice() {
+  voices = speechSynthesis.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map((voice) => (
+      <option value="${voice.name}">
+        ${voice.name}(${voice.lang})
+      </option>
+    ))
+    .join("");
+}
+
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name === voicesDropdown.value);
+}
+
+speechSynthesis.addEventListener("voiceschanged", changeVoice);
+voicesDropdown.addEventListener("change", setVoice);
+speakButton.addEventListener("click", speaker);
+stopButton.addEventListener("click", () => speaker(false));
